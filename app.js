31e4f06560e8f247f0475ecf5d54d57cee0f90dc
JyothikSandeep <101253647+JyothikSandeep@users.noreply.github.com>
2022-12-02 21:13:07 +0530

    BACK

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const express = require("express");
 const app = express();
 //routes
 const routes = require("./tasks/routes/smac");
+// console.log(routes.route);
 //db connection
 const connectDB = require("./tasks/db/connect");
 //.env module
 require("dotenv").config();
+const notFound = require("./tasks/middleware/notfound");
+
 //express.json
 app.use(express.json());
 app.use("/", routes);
+app.use(notFound);
+// console.log(app);
+
 //data base and server connection
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
--- tasks/controller/smac.js
+++ tasks/controller/smac.js
@@ -1,27 +1,30 @@
 const { rawListeners } = require("process");
-const StudentRegistration = require("../models/smac");
-const FacultyRegistration = require("../models/smac");
+const asyncWrapper = require("../middleware/async");
+const { studentRegistration, facultyRegistration } = require("../models/smac");
+// const FacultyRegistration = require("../models/smac");
 
-const postStudentRegistration = async (req, res) => {
-  try {
-    console.log(req.body);
-    const student_details = await StudentRegistration.create(req.body);
-    console.log(student_details);
-    res.status(201).json({ student_details });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-  // res.send("student registration schema")
-};
-const postFacultyRegistration = async (req, res) => {
-  try {
-    const faculty_details = await FacultyRegistration.create(req.body);
-    res.status(201).json({ faculty_details });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-  // res.send('Faculty Registration details')
-};
+const postStudentRegistration = asyncWrapper(async (req, res) => {
+  console.log(req.body);
+  const student_details = await studentRegistration.create(req.body);
+  console.log(student_details);
+  res.status(201).json({ student_details });
+});
+const postFacultyRegistration = asyncWrapper(async (req, res) => {
+  const faculty_details = await facultyRegistration.create(req.body);
+  res.status(201).json({ faculty_details });
+});
+
+// const postStudentRegistration = async (req, res) => {
+//   try {
+//     console.log(req.body);
+//     const student_details = await StudentRegistration.create(req.body);
+//     console.log(student_details);
+//     res.status(201).json({ student_details });
+//   } catch (error) {
+//     res.status(500).json({ msg: error });
+//   }
+//   // res.send("student registration schema")
+// };
 
 module.exports = {
   postStudentRegistration,
